81e83ba943e65cc784c90310128adabb0d854ceb
RSStavale <rafael@stavale.rio>
2018-03-12 10:29:59 -0300

    PCA Analysis

--- packages.R
+++ packages.R
@@ -3,9 +3,11 @@
 #install.packages("reshape")
 #install.packages("stringr")
 #install.packages("BBmisc")
-install.packages("ff")
-install.packages("propagate")
-install.packages("PerformanceAnalytics")
+#install.packages("ff")
+#install.packages("propagate")
+#install.packages("PerformanceAnalytics")
+install.packages("factoextra")
+install.packages("FactoMineR")
 require(stringr)
 require(RPostgreSQL)
 require(tidyverse)
@@ -14,3 +16,14 @@ require(BBmisc)
 require(ff)
 require(PerformanceAnalytics)
 require(propagate)
+
+#to do
+install.packages("MVN")
+
+#reter PCA, analisar, contribuição das cargas fatorias dos primeiros indicadores
+#analisar os primieros 5 fatores de PCA e a carga de cada um nas variáveis
+#rodar contribuiçao de cada variavel
+
+
+require(MVN)
+#shapiro multivariado
--- script.R
+++ script.R
@@ -6,21 +6,6 @@ con <- dbConnect('PostgreSQL',user = 'postgres',
                  dbname = 'Emissions Database')
 dbListTables(con)
 
-#?dbGetQuery
-#table = '"emissions_agriculture_energy"'
-#col = '"Country"'
-#statement =  'select * from "Emissions_Agriculture_Energy" where "Country" = Brazil'
-#query = "select * from Emissions_Agriculture_Energy"
-#emissions = dbGetQuery(con,'select * from emissions_agriculture_burning_crop_residues_final')
-#emissions
-#dim(emissions)
-#?boxplot
-#require(Rcmdr)
-
-#rm(result_table)
-#str(table)
-#head(table)
-
 emissions_agriculture_cultivated_organic_soils <- my.Table.Transposing.Function(dbReadTable(con,"emissions_agriculture_cultivated_organic_soils"))
 emissions_agriculture_burning_crop_residues <- my.Table.Transposing.Function(dbReadTable(con,"emissions_agriculture_burning_crop_residues"))
 emissions_agriculture_burning_savanna <- my.Table.Transposing.Function(dbReadTable(con,"emissions_agriculture_burning_savanna"))
@@ -33,14 +18,7 @@ emissions_agriculture_manure_management <- my.Table.Transposing.Function(dbReadT
 emissions_agriculture_rice_cultivation <- my.Table.Transposing.Function(dbReadTable(con,"emissions_agriculture_rice_cultivation"))
 emissions_agriculture_synthetic_fertilizers <- my.Table.Transposing.Function(dbReadTable(con,"emissions_agriculture_synthetic_fertilizers"))
 
-#require(tidyverse)
-#require(reshape)
-#library(dplyr)
-
-#final <- Reduce(function(x, y) merge(x, y, all=T,by("country","year"), 
- #      mylist, accumulate=F))
 library(tibble)
-
 
 finalTable <-  
   Reduce(function(dtf1, dtf2) merge(dtf1, dtf2, by=c("country","year"), all= TRUE),
@@ -74,46 +52,37 @@ toRename <- finalTable %>%
 namesofcolumns <- names(finalTable)
 namesofcolumns <- str_replace_all(namesofcolumns,"\\.y$","")
 names(finalTable) <- namesofcolumns
-
+#Merge de tabela individualmente
 finalTable <-merge(x =  finalTable, y = emissions_agriculture_burning_crop_residues, by=c("country","year"), all= TRUE)
-
-
-
-
-
-
-install.packages("ff")
-require(ff)
-install.packages("vegan")
-
+#---------------------------------------------------
 
 median_Table <- aggregate(finalTable, by=list(finalTable$country),  FUN=median, na.rm = TRUE)
 #warnings()
+normalized_median_table <- normalize(median_table_values, method = "range", range = c(-3,3),margin = 1L, on.constant = "quiet")
+#as_tibble(median_table)
+as_tibble(normalized_median_table)
+normalized_median_table_values <- normalized_median_table[,3:1154]
+#rm(normalized_median_table)
+library(dplyr)
+scaled_median_table_values %>%
+  mutate_all(as.double())
+
+normalized_median_table_values <- sapply( normalized_median_table_values, as.numeric )
+
+#cormatrix <- cor(use = "pairwise.complete.obs", x = normalized_median_table_values,method = "spearman")
+
+require(Rcmdr)
+#require(tibble)
+
+normalized_median_table_values <- as.tibble(normalized_median_table_values)
+median_table_values <- median_table[,4:1155]
+
+scale(median_table_values)
+scaled_median_table <- median_table_values
 
 normalized_median_table <- normalize(median_table, method = "range", range = c(-3,3),margin = 1L, on.constant = "quiet")
-normalized_median_table$year <- NULL
-cor(normalized_median_table)
-require(Rcmdr)
-#Normalized_median_table_deconstand <- decostand(medianTable, method = "standardize", MARGIN = 1L, range.global, na.rm=TRUE)
-require(tibble)
-as_tibble(median_table)
-as_tibble(normalized_median_table)
-normalized_median_table_values <- normalized_median_table[,4:1155]
-#rm(normalized_median_table_values)
-library(dplyr)
-#normalized_median_table_values %>%
-#  mutate_all(as.double())
-#?propagate::bigcor
-#chart.Correlation(normalized_median_table[,3:1154], histogram=TRUE, pch=19)
-#normalized_median_table_values <- sapply( normalized_median_table_values, as.numeric )
 
-#?ff
-#matrixFF <- as.ffdf(normalized_median_table[,4:1155])
-#matrixFF
-#COR <- propagate::bigcor(matrixFF, size = 2000,fun ="cor")
-#?cor
-cormatrix <- cor(use = "pairwise.complete.obs", x = normalized_median_table_values,method = "spearman")
 
-corframe <- as.data.frame(cormatrix)
+str(scaled_median_table)
+mvn(scaled_median_table, mvnTest = "royston", univariatePlot = "qqplot")
 
-corframe
